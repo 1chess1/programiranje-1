ab7943e529db12750d4252c730bee79bc522cb39
1chess1 <138295575+1chess1@users.noreply.github.com>
2023-08-22 17:46:58 +0200

    dodajanje funkcije za pobiranje vseh podatkov
    samo dodatek, če oseba želi pobrati vse podatke in ne samo prvi 50 strani

--- pobiranje_podatkov.py
+++ pobiranje_podatkov.py
@@ -7,18 +7,33 @@ def pobiranje_podatkov(link):
     req1 = requests.get(link, headers=headers).text
     return req1
 
-def v_datoteko(st):
+def v_datoteko(st_strani):
     vsebina = ""
     link2 = "https://mangareader.to/az-list?page="
-    i = 1
+    stevec = 1
     with open("vsebina.html", "w") as shranjena_vsebina:
-        while i < st+1:
+        while stevec < st+1:
             try:
                 vsebina = pobiranje_podatkov(link2)              
-                link2 = "https://mangareader.to/az-list?page="+str(i)
+                link2 = "https://mangareader.to/az-list?page="+str(stevec)
                 shranjena_vsebina.write(vsebina)
             except:
                 continue
-            i += 1
+            stevec += 1
     return None
+
+def poberi_vse(): #če želiš pobrati čisto vse podatke
+    vsebina = ""
+    link2 = "https://mangareader.to/az-list?page="
+    stevec = 1
+    with open("vsebina.html", "w") as shranjena_vsebina:
+        while True:
+            try:
+                vsebina = pobiranje_podatkov(link2)              
+                link2 = "https://mangareader.to/az-list?page="+str(stevec)
+                shranjena_vsebina.write(vsebina)
+            except:
+                if stevec > 650:
+                    return None
+            stevec += 1
 v_datoteko(639)
