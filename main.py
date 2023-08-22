cb50c89ece4eeb7fb75ed041949b898f8df37b97
1chess1 <138295575+1chess1@users.noreply.github.com>
2023-08-22 17:48:56 +0200

    dodajanje komentarja

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from shranjevanje_v_csv import shranjevanje_v_csv
 from pobiranje_podatkov import v_datoteko
 from izsluscenje_podatkov import pobiranje_podatkov
                #samo poženeš vse sem spravil v en program, da ni potrebno dpirati in zaganjati treh različnih
-v_datoteko(50) #odločil sem se, da bom naložil prvih 50 strani(lahko jih tudi več samo potem shranjevanje in obdelava podatkov trajo malo dlje)
-pobiranje_podatkov()
+v_datoteko(50) #odločil sem se, da bom naložil prvih 50 strani, če želiš vse nadomesti z "poberi_vse()"
+pobiranje_podatkov() 
 shranjevanje_v_csv()
