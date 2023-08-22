d303e0c97fd1e473820279f74daad7b36878a673
1chess1 <138295575+1chess1@users.noreply.github.com>
2023-08-22 16:27:19 +0200

    dodajanje navodil
    dodajanje navodil

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from shranjevanje_v_csv import shranjevanje_v_csv
 from pobiranje_podatkov import v_datoteko
 from izsluscenje_podatkov import pobiranje_podatkov
-
+               #samo poženeš vse sem spravil v en program, da ni potrebno dpirati in zaganjati treh različnih
 v_datoteko(50) #odločil sem se, da bom naložil prvih 50 strani(lahko jih tudi več samo potem shranjevanje in obdelava podatkov trajo malo dlje)
 pobiranje_podatkov()
 shranjevanje_v_csv()
